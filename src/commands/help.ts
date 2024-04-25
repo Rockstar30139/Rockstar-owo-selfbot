82416b16a618c557249e56788ee66371e59a0fe4
Rockstar30139 <147255392+Rockstar30139@users.noreply.github.com>
2024-04-25 21:18:36 +0530

    help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,7 +7,7 @@ export default {
         let document = ""
         const listCommand = Object.keys(global.commands)
         for(const command of listCommand) document += `**${command}:** ${global.commands[command].info}\n`
-        document += "Join Our Support Server For Help: https://discord.gg/Yr92g5Zx3e"
+        document += "Join Our Support Server For Help: https://discord.com/invite/D28N9Q2xN9"
         if(args[0]) message.reply(
             listCommand.includes(args[0]) 
             ? `**${args[0]}:** ${global.commands[args[0]].info}`
